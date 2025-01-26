83891fcd531392ec0df0b8f6d00f48cbfd8a513a
Haurio Vieira <hauriovieira@gmail.com>
2025-01-26 16:22:01 +0000

    Configurando mensagem de cadastro e adição dos campos cargo e loja

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -12,9 +12,13 @@ export class DefaultLoginLayoutComponent {
   currentYear: number = new Date().getFullYear();
   isModalVisible: boolean = false; // Variável para controlar a visibilidade do modal
   isSuccessPopupVisible: boolean = false; // Variável para controlar a visibilidade do popup de sucesso
+  isEmailRegisteredPopupVisible: boolean = false; // Variável para controlar a visibilidade do popup de email já registrado
+  lojas: string[] = []; // Lista de lojas (preenchido dinamicamente)
+  cargos: string[] = []; // Lista de cargos (preenchido dinamicamente)
 
   openRegisterModal(): void {
     this.isModalVisible = true;
+    this.carregarDados(); // Carregar dados ao abrir o modal
   }
 
   closeRegisterModal(): void {
@@ -25,17 +29,40 @@ export class DefaultLoginLayoutComponent {
     this.isSuccessPopupVisible = true;
     setTimeout(() => {
       this.isSuccessPopupVisible = false;
-    }, 3000); // Popup será exibido por 3 segundos
+    }, 5000); // Popup será exibido por 5 segundos
+  }
+
+  showEmailRegisteredPopup(): void {
+    this.isEmailRegisteredPopupVisible = true;
+    setTimeout(() => {
+      this.isEmailRegisteredPopupVisible = false;
+    }, 5000); // Popup será exibido por 5 segundos
   }
 
   onRegisterSubmit(event: Event): void {
     event.preventDefault(); // Prevenir comportamento padrão de submissão
-    console.log('Formulário de cadastro enviado!');
-    this.isModalVisible = false;
-    this.showSuccessPopup(); // Mostrar popup de sucesso
-
-    // Limpar campos do formulário
     const form = event.target as HTMLFormElement;
-    form.reset();
+    const emailInput = form.querySelector('input[type="email"]') as HTMLInputElement;
+    const email = emailInput.value;
+
+    if (this.isEmailRegistered(email)) {
+      this.showEmailRegisteredPopup();
+    } else {
+      console.log('Formulário de cadastro enviado!');
+      this.isModalVisible = false;
+      this.showSuccessPopup(); // Mostrar popup de sucesso
+      form.reset();
+    }
+  }
+
+  isEmailRegistered(email: string): boolean {
+    // Simulação de verificação no banco de dados (substitua com sua lógica real)
+    const registeredEmails = ['example@example.com', 'user@domain.com']; // Emails registrados fictícios
+    return registeredEmails.includes(email);
+  }
+
+  carregarDados(): void {
+    this.lojas = ['Loja 1', 'Loja 2', 'Loja 3'];
+    this.cargos = ['Gerente', 'Atendente', 'Caixa'];
   }
 }
